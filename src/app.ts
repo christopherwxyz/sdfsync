c44f21b2ad4da62ef7a5b9d1021fbb8858449085
Mark Keaton <mkeaton@gmail.com>
2022-10-26 10:39:49 -0500

    Add env var token save. Needs JDK.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,30 @@
+import { env } from "process";
 import * as shell from "shelljs";
 
 console.log("Hello world");
 
+const saveToken = () => {
+  const commandList = [
+    "suitecloud",
+    "account:savetoken",
+    "--account",
+    env.ACCOUNT,
+    "--authid",
+    env.AUTHID,
+    "--tokenid",
+    env.TOKENID,
+    "--tokensecret",
+    env.TOKENSECRET,
+    "--url",
+    env.URL,
+  ];
+  const command = commandList.join(" ");
+  shell.exec(command);
+};
+
 export const handler = () => {
   console.log("Inside lambda handler");
-  shell.exec("suitecloud --help");
+  saveToken();
+  // shell.exec("suitecloud --help");
   return {};
 };
