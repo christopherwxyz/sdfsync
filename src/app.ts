6c1bc70d083aa45e8bdd117a9596a827ccbd2918
Mark Keaton <mkeaton@gmail.com>
2022-10-26 09:59:51 -0500

    Adds shelljs dependency.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import * as shell from "shelljs";
+
 console.log("Hello world");
 
 export const handler = () => {
   console.log("Inside lambda handler");
+  shell.exec("suitecloud --help");
   return {};
 };
