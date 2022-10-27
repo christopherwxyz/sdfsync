af6f77dd62760ddaecc8c44d2e19b90385c63a28
Mark Keaton <mkeaton@gmail.com>
2022-10-27 10:13:21 -0500

    Sets up SSH and is able to clone repository.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,38 @@
+import * as fs from "fs/promises";
 import { env } from "process";
 import * as shell from "shelljs";
 
-console.log("Hello world");
+/*
+Assumes an ed25519 private key delimited by raw \n newlines
+ */
+const formatDeployKey = (key: string) =>
+  key.replaceAll(`"`, ``).replaceAll(`\\n`, `\n`);
 
-const saveToken = () => {
+const getHostConfig = () => `
+Host github.com-alias
+  Hostname github.com
+  IdentityFile=/root/.ssh/ed25519
+`;
+
+const setupSsh = async () => {
+  const privateKey = formatDeployKey(env.DEPLOYKEY);
+  const hostConfig = getHostConfig();
+  shell.cd("/root");
+  await shell.mkdir("/root/.ssh");
+  await fs.writeFile("/root/.ssh/ed25519", privateKey);
+  await fs.writeFile("/root/.ssh/config", hostConfig);
+  shell.chmod(600, "/root/.ssh/ed25519");
+  shell.exec("ssh -o StrictHostKeyChecking=no git@github.com-alias");
+};
+
+const cloneRepo = async () => {
+  shell.cd("/tmp");
+  shell.exec("git clone git@github.com-alias:constituentvoice/ns.git");
+  shell.cd("/tmp/ns");
+  shell.exec("ls -al");
+};
+
+const saveNetSuiteToken = () => {
   const commandList = [
     "suitecloud",
     "account:savetoken",
@@ -22,9 +51,10 @@ const saveToken = () => {
   shell.exec(command);
 };
 
-export const handler = () => {
-  console.log("Inside lambda handler");
-  saveToken();
+export const handler = async () => {
+  await setupSsh();
+  await cloneRepo();
+  saveNetSuiteToken();
   shell.exec("suitecloud file:list --folder /SuiteScripts");
   return {};
 };
